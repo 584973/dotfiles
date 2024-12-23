874e522f28f0cc43c691655b316ce10f83da9333
Håkon Herrevold <hakonherrevold@Hakons-MacBook-Pro.local>
2024-12-23 19:47:57 +0100

    Toggle en terminal på bunn

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,5 +13,19 @@ vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("costum-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
+})
+vim.keymap.set("n", "<space>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 10)
+end)
+
 require("vim-options")
 require("lazy").setup("plugins")
