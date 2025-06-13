ac9d3341b3b2e6242cc1d291ab9cf759653bb970
Håkon Herrevold <hakonherrevold@Hakons-MacBook-Pro.local>
2025-06-13 23:15:30 +0200

    (ideavim) some shortcut changes

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,12 +1,27 @@
 return {
-	"vim-test/vim-test",
+  "vim-test/vim-test",
   dependencies = {
     "preservim/vimux",
   },
-	vim.keymap.set("n", "<leader>t", ":TestNearest<CR>"),
-	vim.keymap.set("n", "<leader>T", ":TestFile<CR>"),
-	vim.keymap.set("n", "<leader>l", ":TestLast<CR>"),
-	vim.keymap.set("n", "<leader>g", ":TestVisit<CR>"),
-  vim.cmd("let test#strategy = 'vimux'"),
-  vim.cmd("let g:test#javascript#runner = 'jest'"),
+  config = function()
+    -- Keymaps for default tests (using Jest)
+    vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
+    vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
+    vim.keymap.set("n", "<leader>l", ":TestLast<CR>")
+    vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
+
+    -- Strategy
+    vim.cmd("let test#strategy = 'vimux'")
+
+    -- Default JavaScript runner (Jest)
+    vim.cmd("let g:test#javascript#runner = 'jest'")
+
+    -- Custom Playwright command
+    vim.keymap.set("n", "<leader>åp", function()
+      local file = vim.fn.expand("%")
+      local cmd = "npx playwright test " .. file
+      vim.cmd("VimuxRunCommand('" .. cmd .. "')")
+    end, { desc = "Run Playwright test for current file" })
+  end,
 }
+
