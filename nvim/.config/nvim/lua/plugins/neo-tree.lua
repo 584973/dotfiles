7bbeec4ac041950418d34deca7135f0d1d5dada0
Håkon Herrevold <54097912+584973@users.noreply.github.com>
2025-07-17 15:18:36 +0200

    endringer (#6)
    Co-authored-by: Håkon Herrevold <hakonherrevold@Hakons-MacBook-Pro.local>

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,40 +1,33 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          follow_current_file = {
-            enabled = true,
-          },
-          filtered_items = {
-            visible = true,
-            show_hidden_count = true,
-            hide_dotfiles = false,
-            hide_gitignore = false,
-          },
-        },
-      })
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+					},
+					filtered_items = {
+						visible = true,
+						show_hidden_count = true,
+						hide_dotfiles = false,
+						hide_gitignore = false,
+					},
+				},
+			})
 
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader>e",
-        ":Neotree source=filesystem reveal=true position=float toggle=true<CR> ",
-        { noremap = true, silent = true }
-      )
-    end,
-  },
-  {
-    "stevearc/oil.nvim",
-    config = function()
-      require("oil").setup()
-    end,
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
-  },
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>e",
+				":Neotree source=filesystem reveal=true position=float toggle=true<CR> ",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 }
