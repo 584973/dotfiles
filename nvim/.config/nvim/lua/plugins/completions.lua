2cb25b8b0b896ae8200ae3ee5783b3bcb29a7d4d
Håkon Herrevold <54097912+584973@users.noreply.github.com>
2025-07-17 14:03:27 +0200

    Bedrelsp (#5)
    * (nvim) fixing lsp
    
    * (nvim) Some changes
    
    * (nvim) tweaks and kotlin lsp
    
    ---------
    
    Co-authored-by: Håkon Herrevold <hakonherrevold@Hakons-MacBook-Pro.local>

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -2,6 +2,9 @@ return {
   {
     "hrsh7th/cmp-nvim-lsp",
   },
+  {
+    "hrsh7th/cmp-buffer",
+  },
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
@@ -13,7 +16,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
