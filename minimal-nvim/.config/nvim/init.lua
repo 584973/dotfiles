b17d6434c64aad8f6f6268047681a4aa9dabe2df
Mr Herrevold <haakher031@gmail.com>
2026-04-01 17:47:09 +0200

    (minimal-nvim) tidy up

--- minimal-nvim/.config/nvim/init.lua
+++ minimal-nvim/.config/nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 
+-- Options
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -10,13 +11,27 @@ vim.opt.scrolloff = 8
 vim.opt.conceallevel = 2
 vim.opt.termguicolors = true
 vim.opt.undofile = true
-
--- Indenting lines
 vim.opt.list = true
 vim.opt.listchars = {
-	tab = "| ",
-	leadmultispace = "|   ",
+  tab = "| ",
+  leadmultispace = "|   ",
 }
+
+-- Plugins
+vim.pack.add({
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/stevearc/oil.nvim",
+  "https://github.com/rose-pine/neovim",
+})
+
+-- Colorscheme
+vim.cmd.colorscheme("rose-pine")
+
+-- Oil
+require("oil").setup({
+  view_options = { show_hidden = true },
+})
+vim.keymap.set("n", "e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Auto pairs
 vim.keymap.set("i", "(", "()<Left>")
@@ -25,23 +40,11 @@ vim.keymap.set("i", "{", "{}<Left>")
 vim.keymap.set("i", '"', '""<Left>')
 vim.keymap.set("i", "'", "''<Left>")
 
-
-vim.pack.add({
-'https://github.com/neovim/nvim-lspconfig',
-'https://github.com/stevearc/oil.nvim',
-'https://github.com/rose-pine/neovim',
-})
-
-vim.cmd.colorscheme('rose-pine')
-
-require("oil").setup()
-vim.keymap.set("n", "e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
 -- Save and quit
-vim.keymap.set("n", "<leader>q", ":q<cr>")
 vim.keymap.set("n", "<leader>w", ":w<cr>")
+vim.keymap.set("n", "<leader>q", ":q<cr>")
 
--- Navigation
+-- Buffers
 vim.keymap.set("n", "<leader>n", ":bn<cr>")
 vim.keymap.set("n", "<leader>p", ":bp<cr>")
 vim.keymap.set("n", "<leader>x", ":bd<cr>")
@@ -50,18 +53,17 @@ vim.keymap.set("n", "<leader>x", ":bd<cr>")
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
 
--- Tmux nav
+-- Tmux navigation
 local function navigate(dir)
-	local nr = vim.fn.winnr()
-	vim.cmd("wincmd " .. dir)
-	if vim.fn.winnr() == nr and vim.env.TMUX then
-		local map = { h = "L", j = "D", k = "U", l = "R" }
-		vim.fn.system("tmux select-pane -" .. map[dir])
-	end
+  local nr = vim.fn.winnr()
+  vim.cmd("wincmd " .. dir)
+  if vim.fn.winnr() == nr and vim.env.TMUX then
+    local map = { h = "L", j = "D", k = "U", l = "R" }
+    vim.fn.system("tmux select-pane -" .. map[dir])
+  end
 end
 
 vim.keymap.set("n", "<C-h>", function() navigate("h") end)
 vim.keymap.set("n", "<C-j>", function() navigate("j") end)
 vim.keymap.set("n", "<C-k>", function() navigate("k") end)
 vim.keymap.set("n", "<C-l>", function() navigate("l") end)
-
