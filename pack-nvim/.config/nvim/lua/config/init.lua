c21662111b579559ad7a806acbd5f376b263c589
Mr Herrevold <haakher031@gmail.com>
2026-04-02 01:50:58 +0200

    (pack-nvim) reorganized

--- pack-nvim/.config/nvim/lua/config/init.lua
+++ pack-nvim/.config/nvim/lua/config/init.lua
@@ -1,2 +1,43 @@
-require("config.options")
-require("config.keymaps")
+-- Options
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.conceallevel = 2
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "| ",
+  leadmultispace = "|   ",
+}
+
+-- Auto pairs
+vim.keymap.set("i", "(", "()<Left>")
+vim.keymap.set("i", "[", "[]<Left>")
+vim.keymap.set("i", "{", "{}<Left>")
+vim.keymap.set("i", '"', '""<Left>')
+vim.keymap.set("i", "'", "''<Left>")
+
+-- Save and quit
+vim.keymap.set("n", "<leader>w", ":w<cr>")
+vim.keymap.set("n", "<leader>q", ":q<cr>")
+
+-- Save to clipboard
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
+
+-- Buffers
+vim.keymap.set("n", "<leader>n", ":bn<cr>")
+vim.keymap.set("n", "<leader>p", ":bp<cr>")
+vim.keymap.set("n", "<leader>x", ":bd<cr>")
+
+-- Lazygit
+vim.keymap.set("n", "<leader>lg", ":LazyGit<cr>", { silent = true })
+
+-- Move lines
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
--- pack-nvim/.config/nvim/lua/lsp/init.lua
+++ pack-nvim/.config/nvim/lua/lsp/init.lua
@@ -1,1 +1,8 @@
 vim.lsp.enable({ "lua_ls", "gopls", "pyright" })
+
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, {})
+vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {})
