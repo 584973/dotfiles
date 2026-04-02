2e8ad6091135c73cdda8c8fb61d3808be37f798d
Mr Herrevold <haakher031@gmail.com>
2026-04-02 12:38:26 +0200

    (nvim) added yank to clipboard keymaps

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -24,6 +24,10 @@ vim.keymap.set("n", "<leader>n", ":bn<cr>")
 vim.keymap.set("n", "<leader>p", ":bp<cr>")
 vim.keymap.set("n", "<leader>x", ":bd<cr>")
 
+-- Save to clipboard
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
+
 -- Move lines
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
