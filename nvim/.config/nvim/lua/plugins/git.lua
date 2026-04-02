44cfccff245dbb5e6491e7f0ad004fbd0a63a5d0
Mr Herrevold <haakher031@gmail.com>
2026-04-02 12:17:06 +0200

    (nvim) reorganized

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,31 +1,16 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    end,
-  },
-  {
-    "tpope/vim-fugitive",
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -5,9 +5,4 @@ return {
 	build = function()
 		vim.fn["mkdp#util#install"]()
 	end,
-	keys = {
-		{ "<leader>ma", ":MarkdownPreview<CR>", desc = "Markdown: start preview" },
-		{ "<leader>mc", ":MarkdownPreviewStop<CR>", desc = "Markdown: stop preview" },
-		{ "<leader>mt", ":MarkdownPreviewToggle<CR>", desc = "Markdown: toggle preview" },
-	},
 }
