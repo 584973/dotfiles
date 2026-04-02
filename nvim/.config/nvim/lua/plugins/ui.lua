2ffde5077e98d5c716ae8d29b3ce1e0553a61a7d
Mr Herrevold <haakher031@gmail.com>
2026-04-02 12:22:29 +0200

    (nvim) split flash into its own file instead of ui

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -120,20 +120,4 @@ return {
 			})
 		end,
 	},
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-		},
-	},
 }
