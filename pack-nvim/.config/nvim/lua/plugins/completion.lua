61951277d30922c33344097df753fba9a4b53923
hakonherrevold <haakher031@gmail.com>
2026-04-05 14:37:20 +0200

    (pack-nvim) borders

--- pack-nvim/.config/nvim/lua/plugins/completion.lua
+++ pack-nvim/.config/nvim/lua/plugins/completion.lua
@@ -11,8 +11,10 @@ require("blink.cmp").setup({
 		documentation = {
 			auto_show = true,
 			auto_show_delay_ms = 200,
+			window = { border = "rounded" },
 		},
 		menu = {
+			border = "rounded",
 			draw = {
 				columns = {
 					{ "kind_icon" },
